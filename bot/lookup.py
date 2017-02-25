8f66d14b57bdb61f087148cf902cf0889a915c5a
Vladimir Smirnov <synteny@users.noreply.github.com>
2017-02-25 20:57:01 +0300

    bot

---
+++ bot/lookup.py
@@ -0,0 +1,61 @@
+import requests
+import json
+
+def request_synonyms(oid, iri):
+    j = requests.get('http://www.ebi.ac.uk/ols/api/ontologies/' + oid + '/terms?iri=' + iri).json()
+    return j["_embedded"]["terms"][0]["synonyms"] if "_embedded" in j else []
+
+
+def search_others(query, doids):
+    j = requests.get('http://www.ebi.ac.uk/ols/api/search?q=' + query + '&ontology=efo, hp, ogms, ordo').json()
+
+    results = []
+    syns = set()
+    for item in j['response']['docs']:
+        if 'iri' in item:
+            ss = request_synonyms(item['ontology_name'], item['iri'])
+            if ss:
+                for s in ss:
+                    syns.add(s.lower().strip())
+
+    for syn in syns:
+        r = search_doid(syn, True, doids)
+        if len(r) > 0:
+            results.append(r)
+
+    return results
+
+
+def search_doid(query, exact, doids):
+    j = requests.get('http://www.ebi.ac.uk/ols/api/search?q=' + query + '&ontology=doid&exact=' + str(exact).lower()).json()
+
+    matches = []
+    for item in j['response']['docs']:
+        if 'obo_id' in item and item['obo_id'] not in doids:
+            matches.append({
+                             'id':item['obo_id'],
+                             'desc':next(iter(item['description'])) if 'description' in item else '',
+                             'label':item['label'],
+                            })
+            doids.add(item['obo_id'])
+
+    return matches
+
+
+def search(query):
+    doids = set()
+    doid_results = search_doid(query, False, doids)
+    other_results = search_others(query, doids)
+    return {'query': query, 'top_matches': doid_results, 'synonym_matches': other_results}
+
+
+def parse(text):
+    lines = list(text.split('\n'))
+    def getTerms(line):
+        return list(map(lambda s: s.strip(), line.split(':')[1].split(',')))
+    return getTerms(lines[0]) + getTerms(lines[3])
+
+
+def lookup(text):
+    for t in parse(text):
+        yield json.dumps(search(t))
---
+++ bot/main.py
@@ -0,0 +1,57 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+from lookup import *
+
+TOKEN='331304045:AAGVjGkL-11ysUPoSTk-EStFTNB93s00Gxw'
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger(__name__)
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(bot, update):
+    update.message.reply_text('Hi!')
+
+
+def help(bot, update):
+    update.message.reply_text('Help!')
+
+
+def process(bot, update):
+    for m in lookup(update.message.text):
+        bot.sendMessage(update.message.chat_id, m)
+
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+
+def main():
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater(TOKEN)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, process))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
