3bd4fd3c2a01198400be9377a7a09a90e16e0422
Mikhail Shvets <mike.y.shvets@gmail.com>
2017-02-26 10:58:03 +0300

    Rename folder wiki_pubmed_fuzzy

---
+++ wiki_pubmed_fuzzy/ontology.py
@@ -0,0 +1,13 @@
+from disease_ontology.scripts.oboparser import parse
+
+def get_ontology(filename):
+    '''Load ontology, lower-case name and rename single non-ascii disease'''
+    
+    ontology = parse(filename, typedefs=['is_a'])
+    
+    for key in ontology.terms.keys():
+        ontology.terms[key].name = ontology.terms[key].name.lower()
+        if ontology.terms[key].name.startswith('weissenbacher-zw'):
+            ontology.terms[key].name = 'weissenbacher-zweymuller syndrome'
+    
+    return ontology
