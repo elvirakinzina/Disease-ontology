231f6bbd21bfaa5b830c7b39e8c7383a05c44850
Mikhail Shvets <mike.y.shvets@gmail.com>
2017-02-26 13:32:22 +0300

    Add NLT modules

---
+++ NLP/nlp.py
@@ -0,0 +1,23 @@
+from gensim.models import word2vec, doc2vec
+
+def query_to_words(query):
+    return query.lower().split()
+
+def synonyms(query, model_trigram):
+    word = ''
+    words = query_to_words(query)
+    for w in words:
+        word += (w+'_')
+    word = word[:-1]
+    
+    syns = model_trigram.similar_by_word(word)
+    syns = [syn[0] for syn in syns]
+    return syns
+
+def distance(query1, query2, model_doc2vec):
+    
+    doc1 = query_to_words(query1)
+    doc2 = query_to_words(query2)
+    sim = model_doc2vec.n_similarity(doc1, doc2)
+    return sim
+
